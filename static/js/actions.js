1b259377d4bd5cbd617a661cb1d191cf20e4abc8
ValdezCristianCD <cvaldez2@mail.austral.edu.ar>
2025-05-24 21:04:51 -0300

    Feat: Se hace responsive la navbar
    +responsive.css
    +style.css
    +vars.css
    +actions.js
    *base.html
    *navbar.html
    *landing.html

---
+++ static/js/actions.js
@@ -0,0 +1,9 @@
+navbar = document.querySelector('.r-navbar')
+
+shownav.addEventListener('click', () =>{
+    navbar.classList.toggle('deploid')
+})
+
+hidenav.addEventListener('click', () =>{
+    navbar.classList.toggle('deploid')
+})
