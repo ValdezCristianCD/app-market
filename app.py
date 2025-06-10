8a1ec10ed99b8c6d75fe868b7c0bc06240bd3348
Elena Favelukes <efavelukes@mail.austral.edu.ar>
2025-06-10 11:50:03 -0300

    pags

--- app.py
+++ app.py
@@ -66,8 +66,82 @@ def uploadData():
 
     return redirect('/')
 
+@app.route('/personal', methods=['GET'])
+def personal():
+    data = db_path.session.query()
 
+    links = {
+            'LANDING' : '/landing',
+            'CARGAR DATA' : '/loadData'
+        }
+    page_vars = {
+        **app_config,
+        'nav_links' : links,
+        'app_section' : 'Cargar Data' 
+    }
+    return render_template('pages/personal.html',**page_vars)
 
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+
+    links = {
+            'LANDING' : '/landing',
+            'CARGAR DATA' : '/loadData'
+        }
+    page_vars = {
+        **app_config,
+        'nav_links' : links,
+        'app_section' : 'Cargar Data' 
+    }
+
+    if request.method == 'POST':
+        people = request.form.get("people")
+        income = int(request.form.get("income"))
+        if 1 < income < 30000 and people == 1:
+            return "Clase Baja (pobre, negro, villero, asqueroso, suciedad, mono, inferior)"
+        if 1 < income < 42000 and people == 2:
+            return "Clase Baja (pobre, negro, villero, asqueroso, suciedad, mono, inferior)"
+        if 1 < income < 60000 and people == 4:
+            return "Clase Baja (pobre, negro, villero, asqueroso, suciedad, mono, inferior)"
+        if 30000 < income < 40000 and people == 1:
+            return "Clase Baja-alta"
+        if 42000 < income < 56000 and people == 2:
+            return "Clase Baja-alta"
+        if 60000 < income < 80000 and people == 4:
+            return "Clase Baja-alta"
+        if 40000 < income < 55000 and people == 1:
+            return "Clase Media-baja"
+        if 56000 < income < 78000 and people == 2:
+            return "Clase Media-baja"
+        if 80000 < income < 110000 and people == 4:
+            return "Clase Media-baja"
+        if 55000 < income < 90000 and people == 1:
+            return "Clase Media"
+        if 78000 < income < 127000 and people == 2:
+            return "Clase Media"
+        if 110000 < income < 180000 and people == 4:
+            return "Clase Media"
+        if 90000 < income < 130000 and people == 1:
+            return "Clase Media-alta"
+        if 127000 < income < 183000 and people == 2:
+            return "Clase Media-alta"
+        if 260000 < income < 400000 and people == 4:
+            return "Clase Media-alta"
+        if 130000 < income < 200000 and people == 1:
+            return "Clase Alta-baja"
+        if 183000 < income < 280000 and people == 2:
+            return "Clase Alta-baja"
+        if 260000 < income < 400000 and people == 4:
+            return "Clase Alta-baja"
+        if income > 200000 and people == 1:
+            return "Clase Alta"
+        if income > 280000 and people == 2:
+            return "Clase Alta"
+        if income > 400000 and people == 4:
+            return "Clase Alta"
+            
+    return render_template('pages/calculator.html',**page_vars)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
